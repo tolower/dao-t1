1af3cc33adb87e952e0a5b155e90a0e30c13988f
tolower <lower_scoring@126.com>
2016-08-31 23:35:15 +0800

    commit

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 	echo 'hello,world111';
+	//连接本地的 Redis 服务
+   $redis = new Redis();
+   $ip=$_ENV['REDIS_PORT_6379_TCP_ADDR'];
+   $port=$_ENV['REDIS_PORT_6379_TCP_PORT'];
+   echo $ip;
+   echo $port;
+   $redis->connect($ip, $port);
+   echo "Connection to server sucessfully";
+   //设置 redis 字符串数据
+   $redis->set("tutorial-name", "Redis tutorial");
+   // 获取存储的数据并输出
+   echo "Stored string in redis:: " . $redis->get("tutorial-name");
 	phpinfo();
 ?>
